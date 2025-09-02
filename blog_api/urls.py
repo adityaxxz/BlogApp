e39b6d6599a0a759dfbf2b9de771f5fda7180c60
adityaxxz <adityaranjan5995@gmail.com>
2025-09-02 11:27:25 +0530

    start app&api

---
+++ blog_api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+# from .views import PostList, PostDetail
+
+app_name = 'blog_api'
+
+
+urlpatterns = [
+    # path('', PostList.as_view(), name='listcreate'),  #show all posts
+    # path('<int:pk>/', PostDetail.as_view(), name='detailcreate'), #show individual post
+]
